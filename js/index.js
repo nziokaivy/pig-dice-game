6a6ceb4b592501f9513ad2bf065a9dfb526ae051
nziokaivy <nziokaivy@gmail.com>
2018-12-17 01:11:05 +0300

    add a function that can generate random numbers between 1-6 to be able to roll dice

--- js/index.js
+++ js/index.js
@@ -6,4 +6,16 @@ currentScore: 0,
 currentPlayer: 1,
 player1: 0,
 player2: 0,
+
+//random number function when rolling dice
+rollDice: function() {
+  var dice = Math.floor(Math.random() * 6);
+  if (dice === 1) {
+    this.currentScore = 0;
+    this.changeTurn();
+  } else {
+    this.currentScore += dice;
+  }
+  return dice;
+},
 }
