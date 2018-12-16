63eb561553291c2150f91b23455d728ac735ed07
nziokaivy <nziokaivy@gmail.com>
2018-12-17 01:24:55 +0300

    add indendation and spacing to js file

--- js/index.js
+++ js/index.js
@@ -1,48 +1,48 @@
 //business logic
 //game object
 var game = {
-totalScore: 0,
-currentScore: 0,
-currentPlayer: 1,
-player1: 0,
-player2: 0,
+  totalScore: 0,
+  currentScore: 0,
+  currentPlayer: 1,
+  player1: 0,
+  player2: 0,
 
-//random number function when rolling dice
-rollDice: function() {
-  var dice = Math.floor(Math.random() * 6);
-  if (dice === 1) {
-    this.currentScore = 0;
+  //random number function when rolling dice
+  rollDice: function() {
+    var dice = Math.floor(Math.random() * 6);
+    if (dice === 1) {
+      this.currentScore = 0;
+      this.changeTurn();
+    } else {
+      this.currentScore += dice;
+    }
+    return dice;
+  },
+
+  //function for changing turn
+  changeTurn: function() {
+    if (this.currentPlayer === 1) {
+      this.player1 += this.currentScore;
+      this.currentPlayer = 2;
+    } else {
+      this.player2 += this.currentScore;
+      this.currentPlayer = 1;
+    }
+  },
+
+  //function for holding
+  hold: function() {
     this.changeTurn();
-  } else {
-    this.currentScore += dice;
-  }
-  return dice;
-},
+    this.totalScore += this.currentScore;
+    this.currentScore = 0;
+  },
 
-/function for changing turn
-changeTurn: function() {
-  if (this.currentPlayer === 1) {
-    this.player1 += this.currentScore;
-    this.currentPlayer = 2;
-  } else {
-    this.player2 += this.currentScore;
-    this.currentPlayer = 1;
-  }
-},
-
-//function for holding
-hold: function() {
-  this.changeTurn();
-  this.totalScore += this.currentScore;
-  this.currentScore = 0;
-},
-
-//function for checking winner
-winner: function() {
-  if (this.totalScore >= 30) {
-    document.getElementById("playerwin").innerHTML = this.playerName + "is the winner!"
-  }
-},
+  //function for checking winner
+  winner: function() {
+    if (this.totalScore >= 30) {
+      document.getElementById("playerwin").innerHTML = this.playerName + "is the winner!"
+    }
+  },
 }
 
 //user interface
